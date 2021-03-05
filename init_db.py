9e9093c75386891652d76031400df702816e945e
Jasper Lyons <jasper.lyons@rhul.ac.uk>
2021-03-05 17:19:51 +0000

    Suggestions for improving clarity.

--- init_db.py
+++ init_db.py
@@ -4,9 +4,9 @@ DATABASE_FILE = "database.db"
 
 # important:
 #-------------------------------------------------------------
-# This script initialises your database for you using SQLite,
-# just to get you started... there are better ways to express
-# the data you're going to need... especially outside SQLite.
+# This script initialises your SQLite database for you, just
+# to get you started... there are better ways to express the
+# data you're going to need... especially outside SQLite.
 # For example... maybe flag_pattern should be an ENUM (which
 # is available in most other SQL databases), or a foreign key
 # to a pattern table?
@@ -14,15 +14,14 @@ DATABASE_FILE = "database.db"
 # Also... the name of the database (here, in SQLite, it's a
 # filename) appears in more than one place in the project.
 # That doesn't feel right, does it?
-#
 #-------------------------------------------------------------
 
-con = sqlite3.connect(DATABASE_FILE)
+connection = sqlite3.connect(DATABASE_FILE)
 print("- Opened database successfully in file \"{}\"".format(DATABASE_FILE))
 
 # using Python's triple-quote for multi-line strings:
 
-con.execute("""
+connection.execute("""
 
   CREATE TABLE IF NOT EXISTS buggies (
     id                    INTEGER PRIMARY KEY,
@@ -36,16 +35,17 @@ con.execute("""
 
 print("- Table \"buggies\" exists OK")
 
-cur = con.cursor()
+cursor = connection.cursor()
 
-cur.execute("SELECT * FROM buggies LIMIT 1")
-rows = cur.fetchall()
+cursor.execute("SELECT * FROM buggies LIMIT 1")
+rows = cursor.fetchall()
 if len(rows) == 0:
-  cur.execute("INSERT INTO buggies (qty_wheels) VALUES (4)")
-  con.commit()
+  cursor.execute("INSERT INTO buggies (qty_wheels) VALUES (4)")
+  connection.commit()
   print("- Added one 4-wheeled buggy")
 else:
   print("- Found a buggy in the database, nice")
-print("- done")
 
-con.close()
+print("- OK, your database is ready")
+
+connection.close()
