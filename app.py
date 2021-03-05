48d5d636522991cdc6c52651f2a2b7de16166a04
Jasper Lyons <jasper.lyons@rhul.ac.uk>
2021-03-05 17:01:28 +0000

    Some suggestions for clarity!

--- app.py
+++ app.py
@@ -1,56 +1,62 @@
 from flask import Flask, render_template, request, jsonify
 import sqlite3 as sql
+
+# app - The flask application where all the magical things are configured.
 app = Flask(__name__)
 
+# Constants - Stuff that we need to know that won't ever change!
 DATABASE_FILE = "database.db"
 DEFAULT_BUGGY_ID = "1"
-
 BUGGY_RACE_SERVER_URL = "http://rhul.buggyrace.net"
-
 
 #------------------------------------------------------------
 # the index page
 #------------------------------------------------------------
 @app.route('/')
 def home():
-   return render_template('index.html', server_url=BUGGY_RACE_SERVER_URL)
+    return render_template('index.html', server_url=BUGGY_RACE_SERVER_URL)
 
 #------------------------------------------------------------
 # creating a new buggy:
 #  if it's a POST request process the submitted data
 #  but if it's a GET request, just show the form
 #------------------------------------------------------------
-@app.route('/new', methods = ['POST', 'GET'])
-def create_buggy():
-  if request.method == 'GET':
+@app.route('/new', methods = ['GET'])
+def new_buggy():
     return render_template("buggy-form.html")
-  elif request.method == 'POST':
+
+#------------------------------------------------------------
+# 
+#------------------------------------------------------------
+@app.route('/new', methods = ['POST'])
+def create_buggy():
     msg=""
     qty_wheels = request.form['qty_wheels']
     try:
-      with sql.connect(DATABASE_FILE) as con:
-        cur = con.cursor()
+        with sql.connect(DATABASE_FILE) as con:
+            cur = con.cursor()
         cur.execute("UPDATE buggies set qty_wheels=? WHERE id=?", (qty_wheels, DEFAULT_BUGGY_ID))
         con.commit()
         msg = "Record successfully saved"
     except:
-      con.rollback()
-      msg = "error in update operation"
+        con.rollback()
+        msg = "error in update operation"
     finally:
-      con.close()
-      return render_template("updated.html", msg = msg)
+        con.close()
+
+    return render_template("updated.html", msg = msg)
 
 #------------------------------------------------------------
 # a page for displaying the buggy
 #------------------------------------------------------------
 @app.route('/buggy')
 def show_buggies():
-  con = sql.connect(DATABASE_FILE)
-  con.row_factory = sql.Row
-  cur = con.cursor()
-  cur.execute("SELECT * FROM buggies")
-  record = cur.fetchone(); 
-  return render_template("buggy.html", buggy = record)
+    con = sql.connect(DATABASE_FILE)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("SELECT * FROM buggies")
+    record = cur.fetchone(); 
+    return render_template("buggy.html", buggy = record)
 
 #------------------------------------------------------------
 # a placeholder page for editing the buggy: you'll need
@@ -58,9 +64,11 @@ def show_buggies():
 #------------------------------------------------------------
 @app.route('/edit')
 def edit_buggy():
-  return render_template("buggy-form.html")
+    return render_template("buggy-form.html")
 
 #------------------------------------------------------------
+# You probably don't need to edit this... unless you want to ;)
+#
 # get JSON from current record
 #  This reads the buggy record from the database, turns it
 #  into JSON format (excluding any empty values), and returns
@@ -69,41 +77,14 @@ def edit_buggy():
 #------------------------------------------------------------
 @app.route('/json')
 def summary():
-  con = sql.connect(DATABASE_FILE)
-  con.row_factory = sql.Row
-  cur = con.cursor()
-  cur.execute("SELECT * FROM buggies WHERE id=? LIMIT 1", (DEFAULT_BUGGY_ID))
-  return jsonify(
-      {k: v for k, v in dict(zip(
-        [column[0] for column in cur.description], cur.fetchone())).items()
-        if (v != "" and v is not None)
-      }
-    )
+    con = sql.connect(DATABASE_FILE)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("SELECT * FROM buggies WHERE id=? LIMIT 1", (DEFAULT_BUGGY_ID))
 
-#------------------------------------------------------------
-# delete the buggy
-#   Be careful!
-#   If you're tackling task 3-DEL then this code is a starting
-#   place... but it almost certainly doesn't do what you want!
-#   ...you'll need to understand and modify the code to be more
-#   selective about what it's actually doing.
-#------------------------------------------------------------
-@app.route('/delete', methods = ['POST'])
-def delete_buggy():
-  try:
-    msg = "deleting buggy"
-    with sql.connect(DATABASE_FILE) as con:
-      cur = con.cursor()
-      cur.execute("DELETE FROM buggies")
-      con.commit()
-      msg = "Buggy deleted"
-  except:
-    con.rollback()
-    msg = "error in delete operation"
-  finally:
-    con.close()
-    return render_template("updated.html", msg = msg)
+    buggies = dict(zip([column[0] for column in cur.description], cur.fetchone())).items() 
+    return jsonify({ key: val for key, val in buggies if (val != "" and val is not None) })
 
-
+# You shouldn't need to add anything below this!
 if __name__ == '__main__':
-   app.run(debug = True, host="0.0.0.0")
+    app.run(debug = True, host="0.0.0.0")
